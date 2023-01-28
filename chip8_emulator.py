a63c043681337177bb8686e17896730d87b18e70
Rinat Kurbanov <rkurbanov@cloudlinux.com>
2023-01-29 00:57:16 +0500

    Emulator added

--- chip8_emulator.py
+++ chip8_emulator.py
@@ -1,32 +1,29 @@
 import sys
 
-from CHIP8 import CHIP8
 import pygame
+
+from CHIP8 import CHIP8
 
 WHITE = [255, 255, 255]
 BLACK = [0, 0, 0]
+SIZE = 20
 
 
 def main_loop():
-    size = 20
     clock = pygame.time.Clock()
     cpu = CHIP8()
     try:
         filename = sys.argv[1]
+        cpu.load_program(filename)
     except IndexError:
         print("Error: enter program filename to load")
         sys.exit()
-    # cpu.load_program("./programs/demo/Breakout.ch8")
-    # cpu.load_program("./programs/demo/Chip8_Logo.ch8")
-    # cpu.load_program("./programs/demo/Chip8_Picture.ch8")
-    # cpu.load_program("./programs/demo/Clock.ch8")
-    # cpu.load_program("./programs/demo/IBMLogo.ch8")
-    # cpu.load_program("./programs/demo/Zero_Demo.ch8")
-    cpu.load_program("./programs/games/Space_Flight.ch8")
-    screen = pygame.display.set_mode([cpu.screen_width * size, cpu.screen_height * size])
+    screen = pygame.display.set_mode(
+        [cpu.screen_width * SIZE, cpu.screen_height * SIZE]
+    )
     pygame.time.set_timer(pygame.USEREVENT + 1, int(1000 / 60))
     while True:
-        clock.tick(300)
+        clock.tick(200)
         cpu.key_input()
         cpu.execute()
         cpu.decrement_timers()
@@ -34,15 +31,13 @@ def main_loop():
 
 
 def display(screen, screen_matrix):
-    size = 20
     for i in range(0, len(screen_matrix)):
         for j in range(0, len(screen_matrix[0])):
-            cellColor = BLACK
-
+            pixel_color = BLACK
             if screen_matrix[i][j] == 1:
-                cellColor = WHITE
+                pixel_color = WHITE
 
-            pygame.draw.rect(screen, cellColor, [j * size, i * size, size, size], 0)
+            pygame.draw.rect(screen, pixel_color, [j * SIZE, i * SIZE, SIZE, SIZE], 0)
 
     pygame.display.flip()
 
